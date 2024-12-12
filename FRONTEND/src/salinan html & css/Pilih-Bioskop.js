3ad9b0cdd0e1de376043c90890f00de9fc97dacf
Your Name <you@example.com>
2024-12-13 03:36:56 +0700

    menghapus folder salinan html & css

--- FRONTEND/src/salinan html & css/Pilih-Bioskop.js
+++
@@ -1,9 +0,0 @@
-let ticketPrice = 0;
-
-    function setPrice(harga) {
-      ticketPrice = harga;
-      document.getElementById('harga').textContent = ticketPrice;
-      console.log(`Price set to: $${ticketPrice}`); // Useful for debugging
-    }
-
-
--- FRONTEND/src/salinan html & css/pembayaran.js
+++
@@ -1,7 +0,0 @@
-let walletChosen = "";
-
-    function setWallet(wallet) {
-        walletChosen = wallet;
-        document.getElementById('wallet').textContent = walletChosen;
-        console.log("${walletChosen} has been chosen");
-    }
